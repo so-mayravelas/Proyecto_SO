915c0ef7377508c79a83d881bfdfd476de14b5a8
Paola Vela <mayra.paola.vela@estudiant.upc.edu>
2020-10-25 00:36:18 +0200

    Mayra Visio1

---
+++ Juego_version_1/Juego_version_1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Juego_version_1
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Conectar_button1_Click(object sender, EventArgs e)
+        {
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+
+
+            //Creamos el socket 
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+            }
+
+
+            catch (SocketException)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            } 
+        
+        }
+        //Nos desconectamos del servidor
+        private void Desconectar_button2_Click(object sender, EventArgs e)
+        {
+            // Se terminó el servicio. 
+            string mensaje = "0/";
+
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            // Nos desconectamos
+            this.BackColor = Color.Gray;
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+        }
+      
+        //Para que un usuario se pueda registrar
+        private void Registro_button3_Click(object sender, EventArgs e)
+        {
+            //Cuando un usuario quiera registrarse le tendremos que agregar a la BBDD 
+            string mensaje = "1/" + usuario_textBox1 + "/" + contraseña_textBox2;
+            // Enviamos al servidor el usuario que queremos registrar y guardar en BBDD
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            //Recibimos la respuesta del servidor para saber si nos hemos registrado
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+            MessageBox.Show(mensaje);
+
+        }
+
+        //Si ya estamos registardos o nos acabos de registra ya podemos ingresar ya estaremso dados de alta en la BBDD
+        private void Iniciar_button4_Click(object sender, EventArgs e)
+        {
+            // Quiere realizar un inicio de sesión
+            string mensaje = "2/" + usuario_textBox1 + "/" + contraseña_textBox2 ;
+            // Enviamos al servidor el nombre del usuario y la contraseña
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            //Recibimos la respuesta del servidor
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+            MessageBox.Show(mensaje);
+        }
+        //Consulta Galder
+        //No me acuerdo bien la consulta
+        private void Consulta_Galder_CheckedChanged(object sender, EventArgs e)
+        {
+            // Realizamos la consulta escogida
+            string mensaje = "3/" + nombre_textBox3;
+            // Enviamos al servidor el nombre del usuario y la contraseña
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            //Recibimos la respuesta del servidor
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+            MessageBox.Show(mensaje);
+        }
+        //Consulta Mayra 
+        //Consultamos el número de partidas jugadas por el usuario"Pepito que esta en el nombre"
+        private void Consulta_Mayra_CheckedChanged(object sender, EventArgs e)
+        {
+            // Quiere realizar la consulta escogida
+            string mensaje = "4/" + nombre_textBox3;
+            // Enviamos al servidor el nombre del usuario y la contraseña
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            //Recibimos la respuesta del servidor
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+            MessageBox.Show(mensaje);
+        }
+        //Consulta Andoni
+        //No me acuerdo la consulta completa
+        private void Consulta_Andoni_CheckedChanged(object sender, EventArgs e)
+        {
+            // Quiere realizar la consulta escogida
+            string mensaje = "5/" + nombre_textBox3;
+            // Enviamos al servidor el nombre del usuario y la contraseña
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            //Recibimos la respuesta del servidor
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+            MessageBox.Show(mensaje);
+        }
+    }
+}
